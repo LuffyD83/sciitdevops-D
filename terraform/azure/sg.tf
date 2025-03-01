7337e87cc660a50510689338f94c144e8df45b3a
Darius Petricele <darius.petricele@sitline.ro>
2025-03-01 22:30:10 +0200

    updated azure config

---
+++ terraform/azure/sg.tf
@@ -0,0 +1,29 @@
+# Create Network Security Group and rules
+resource "azurerm_network_security_group" "my_terraform_nsg" {
+  name                = "${random_pet.prefix.id}-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "RDP"
+    priority                   = 1000
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name                       = "web"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
--- terraform/azure/variables.tf
+++ terraform/azure/variables.tf
@@ -16,3 +16,13 @@ variable "project" {
   default     = "devops-tools"
 }
 
+variable "resource_group_location" {
+  default     = "eastus"
+  description = "Location of the resource group."
+}
+
+variable "prefix" {
+  type        = string
+  default     = "win-vm-iis"
+  description = "Prefix of the resource name"
+}
