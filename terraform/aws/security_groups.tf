9f7d7e44ab699e03af589c131d658049fdfd96f3
Darius Petricele <darius.petricele@sitline.ro>
2025-03-14 19:17:40 +0200

    update ingres

--- terraform/aws/security_groups.tf
+++ terraform/aws/security_groups.tf
@@ -53,12 +53,14 @@ resource "aws_security_group" "sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-    ingress {
+  
+  ingress {
     from_port   = 22
     to_port     = 22
-    protocol    = "ssh"
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   egress {
     from_port   = 0
     to_port     = 0
