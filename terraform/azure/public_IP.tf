79a43db80bc49fa0d3d1ac5812298687c6bbe37e
Darius Petricele <darius.petricele@sitline.ro>
2025-03-31 00:35:26 +0300

    added configs for the 2nd vm

--- terraform/azure/public_IP.tf
+++ terraform/azure/public_IP.tf
@@ -5,3 +5,12 @@ resource "azurerm_public_ip" "my_terraform_public_ip" {
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Dynamic"
 }
+
+
+# Create public IPs
+resource "azurerm_public_ip" "my_terraform_public_ip_2" {
+  name                = "DevOpsPublicIP_2"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+}
--- terraform/azure/virtual_machine.tf
+++ terraform/azure/virtual_machine.tf
@@ -82,7 +82,7 @@ resource "azurerm_linux_virtual_machine" "my_vm" {
   name                  = "WebVM"
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
-  network_interface_ids = [azurerm_network_interface.my_terraform_nic.id]
+  network_interface_ids = [azurerm_network_interface.my_terraform_nic_2.id]
   size                  = "Standard_DS1_v2"
   admin_username        = var.username
 
